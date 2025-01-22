b87117bf6a6b174e0628e2a5279beb1f1924ef60
Clovis Tessier <46059118+clovistessier@users.noreply.github.com>
2025-01-21 23:04:03 -0800

    Added getters for channel, controller number and value, note, velocity

--- MidiMessage.java
+++ MidiMessage.java
@@ -1,3 +1,5 @@
+package com.midihandler;
+
 public class MidiMessage {
 
     private byte byte1;
@@ -45,5 +47,51 @@ public class MidiMessage {
     public MidiMessageType getMessageType() {
         int statusByte = this.byte1 & 0xF0; // Mask to get the command
         return MidiMessageType.fromStatusByte(statusByte); //Use the static method from the enum
+    }
+
+    public int getChannel() {
+        return (byte1 & 0x0F) + 1; // Channel is in the lower 4 bits, 1-indexed
+    }
+
+    public int getControllerNumber() {
+        if (getMessageType() == MidiMessageType.CONTROL_CHANGE) {
+            return byte2 & 0xFF; // Treat as unsigned
+        }
+        return -1; // Not a Control Change message
+    }
+
+    public int getControllerValue() {
+        if (getMessageType() == MidiMessageType.CONTROL_CHANGE) {
+            return byte3 & 0xFF; // Treat as unsigned
+        }
+        return -1; // Not a Control Change message
+    }
+
+    public int getNote() {
+        if (getMessageType() == MidiMessageType.NOTE_ON || getMessageType() == MidiMessageType.NOTE_OFF) {
+            return byte2 & 0xFF; // Treat as unsigned
+        }
+        return -1; // Not a Note On or Note Off message
+    }
+
+    public int getVelocity() {
+        if (getMessageType() == MidiMessageType.NOTE_ON || getMessageType() == MidiMessageType.NOTE_OFF) {
+            return byte3 & 0xFF; // Treat as unsigned
+        }
+        return -1; // Not a Note On or Note Off message
+    }
+    @Override
+    public String toString() {
+        return "MidiMessage{" +
+                "type=" + getMessageType() +
+                ", channel=" + getChannel() +
+                ", byte1=0x" + String.format("%02X", byte1) +
+                ", byte2=0x" + String.format("%02X", byte2) +
+                ", byte3=0x" + String.format("%02X", byte3) +
+                ", controllerNumber=" + getControllerNumber() +
+                ", controllerValue=" + getControllerValue() +
+                ", note=" + getNote() +
+                ", velocity=" + getVelocity() +
+                '}';
     }
 }
